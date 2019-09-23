d8ad7d27c249f912ec99df54318eba87e19efb06
Chernushov88 <Chernushov88@mail.ru>
2019-09-23 14:00:14 +0300

    create error page (page 404)

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,22 +4,18 @@ import { Link } from 'react-router';
 export default class Header extends React.Component {
     
     static path = '/';
-    className;
     
     render() {
         return (
-            <nav this.className='navbar navbar-default'>
-                <div
-        let className;
-        className='container-fluid'>
+            <nav className='navbar navbar-default'>
+                <div className='container-fluid'>
                     <div className='navbar-header'>
-                        <a className='navbar-brand'
-        let href;
-        href='#'>Brand</a>
+                        <a className='navbar-brand' href='#'>Brand</a>
                     </div>
                     <ul className='nav navbar-nav'>
                         <li><Link to='/'>Home</Link></li>
                         <li><Link to='/contact'>Contact</Link></li>
+                        <li><Link to='/contacts'>Error Page</Link></li>
                     </ul>
                 </div>
             </nav>
